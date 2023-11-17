d62a843a0b4760fd1c9fba83abb7d7415358f266
Cyber Beez <150989206+cyberbeez@users.noreply.github.com>
2023-11-16 22:10:47 -0500

    comment debian-iptables.sh

--- debian-iptables.sh
+++ debian-iptables.sh
@@ -1,26 +1,40 @@
 #!/bin/bash
-cd ..
-iptables-save > ~/default.iptables.bck
+echo "Running IPTables Script from:"
+pwd
+sleep 2
+iptables-save > default.iptables.bck
 iptables -F
+#
+# DROP POLICY IPv6
 ip6tables -P INPUT DROP
 ip6tables -P OUTPUT DROP
 ip6tables -P FORWARD DROP
+#
 iptables -A INPUT -f -j DROP
+# OPEN INBOUND UNLIMITED:
 iptables -A INPUT -p udp --match multiport --dports 53 -j ACCEPT
 iptables -A INPUT -p tcp --match multiport --dports 53 -j ACCEPT
 iptables -A INPUT -p udp --match multiport --dports 123 -j ACCEPT
 iptables -A INPUT -p udp --match multiport --sports 123 -j ACCEPT
+#
+# DOS PREVENTION
 iptables -A INPUT -p tcp ! --tcp-flags SYN,ACK SYN -m state --state NEW -j DROP
 iptables -A INPUT -p udp -m state NEW -m recent --set --name DEFAULT --mask 255.255.255.255 --rsource -j DROP
 iptables -A INPUT -p udp -m state NEW -m recent --update -- seconds 10 --hitcound 20 --name DEFAULT --mask 255.255.255.255 --rsoure
 iptables -A INPUT -p tcp -m state NEW -m recent --set --name DEFAULT --mask 255.255.255.255 --rsource -j DROP
 iptables -A INPUT -p tcp -m state NEW -m recent --update -- seconds 10 --hitcound 20 --name DEFAULT --mask 255.255.255.255 --rsoure
+#
+# RATE LIMITED OPEN INBOUND:
 iptables -A INPUT -p tcp --match multiport --dports 80,443,8089,9997 -j ACCEPT
 iptables -A INPUT -p tcp --match multiport --sports 80,443,8089,9997 -j ACCEPT
 iptables -A INPUT -p icmp -j ACCEPT
+#
+# OPEN OUTBOUND
 iptables -A OUTPUT -f -j DROP
 iptables -A OUTPUT -p tcp ! --tcp-flags SYN,ACK SYN -m state --state NEW -j DROP
 iptables -A OUTPUT -p udp --match multiport --dports 53,123 -j ACCEPT
+#
+#
 iptables -A OUTPUT -p udp --match multiport --sports 53,123 -j ACCEPT
 iptables -A OUTPUT -p tcp --match multiport --dports 53 -j ACCEPT
 iptables -A OUTPUT -p tcp --match multiport --sports 53 -j ACCEPT
@@ -32,8 +46,9 @@ iptables -A INPUT -p icmp -j ACCEPT
 iptables -P FORWARD DROP
 iptables -P INPUT DROP
 iptables -P OUTPUT DROP
-iptables-save > ~/my.iptables.bck
-
+iptables-save > my.iptables.bck
+#
+#
 if ! grep -q "net.ipv6.conf.all.disable_ipv6 = 1" /etc/sysctl.conf; then
   echo "#Disable IPv6
   net.ipv6.conf.all.disable_ipv6 = 1
