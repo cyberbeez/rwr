7046d2499ff46dcdc5688315047dc10eb74f6288
Cyber Beez <150989206+cyberbeez@users.noreply.github.com>
2023-11-16 22:40:27 -0500

    Update debian-iptables.sh
    geared script to Reb/Web/Rev

--- debian-iptables.sh
+++ debian-iptables.sh
@@ -1,32 +1,36 @@
 #!/bin/bash
 echo "Running IPTables Script from:"
 pwd
-sleep 2
-iptables-save > default.iptables.bck
+sleep 1
+iptables-save > b4script.iptables.bck
 iptables -F
 #
-# DROP POLICY IPv6
+# IPv6 DROP POLICY
 ip6tables -P INPUT DROP
 ip6tables -P OUTPUT DROP
 ip6tables -P FORWARD DROP
 #
+# Flush Input Drop Input Rule if it is shut:
 iptables -A INPUT -f -j DROP
+#
 # OPEN INBOUND UNLIMITED:
 iptables -A INPUT -p udp --match multiport --dports 53 -j ACCEPT
 iptables -A INPUT -p tcp --match multiport --dports 53 -j ACCEPT
 iptables -A INPUT -p udp --match multiport --dports 123 -j ACCEPT
 iptables -A INPUT -p udp --match multiport --sports 123 -j ACCEPT
+-A INPUT -p tcp --match multiport --dports 80,110,143 -j ACCEPT
+-A INPUT -p tcp --match multiport --sports 80,110,143 -j ACCEPT
 #
 # DOS PREVENTION
 iptables -A INPUT -p tcp ! --tcp-flags SYN,ACK SYN -m state --state NEW -j DROP
 iptables -A INPUT -p udp -m state NEW -m recent --set --name DEFAULT --mask 255.255.255.255 --rsource -j DROP
-iptables -A INPUT -p udp -m state NEW -m recent --update -- seconds 10 --hitcound 20 --name DEFAULT --mask 255.255.255.255 --rsoure
+iptables -A INPUT -p udp -m state NEW -m recent --update -- seconds 10 --hitcount 50 --name DEFAULT --mask 255.255.255.255 --rsource
 iptables -A INPUT -p tcp -m state NEW -m recent --set --name DEFAULT --mask 255.255.255.255 --rsource -j DROP
-iptables -A INPUT -p tcp -m state NEW -m recent --update -- seconds 10 --hitcound 20 --name DEFAULT --mask 255.255.255.255 --rsoure
+iptables -A INPUT -p tcp -m state NEW -m recent --update -- seconds 10 --hitcount 50 --name DEFAULT --mask 255.255.255.255 --rsource
 #
 # RATE LIMITED OPEN INBOUND:
-iptables -A INPUT -p tcp --match multiport --dports 80,443,8089,9997 -j ACCEPT
-iptables -A INPUT -p tcp --match multiport --sports 80,443,8089,9997 -j ACCEPT
+iptables -A INPUT -p tcp --match multiport --dports 25,80,443 -j ACCEPT
+iptables -A INPUT -p tcp --match multiport --sports 25,80,443 -j ACCEPT
 iptables -A INPUT -p icmp -j ACCEPT
 #
 # OPEN OUTBOUND
@@ -39,10 +43,11 @@ iptables -A OUTPUT -p udp --match multiport --sports 53,123 -j ACCEPT
 iptables -A OUTPUT -p tcp --match multiport --dports 53 -j ACCEPT
 iptables -A OUTPUT -p tcp --match multiport --sports 53 -j ACCEPT
 iptables -A INPUT -p tcp ! --tcp-flags SYN,ACK SYN -m state --state NEW -j DROP
-iptables -A OUTPUT -p tcp --match multiport --dports 80,443,8089,9997 -j ACCEPT
-iptables -A OUTPUT -p tcp --match multiport --sports 80,443,8089,9997 -j ACCEPT
+iptables -A OUTPUT -p tcp --match multiport --dports 80,443,25,80,110,143 -j ACCEPT
+iptables -A OUTPUT -p tcp --match multiport --sports 80,443,25,80,110,143 -j ACCEPT
 iptables -A OUTPUT -p icmp -j ACCEPT
-iptables -A INPUT -p icmp -j ACCEPT
+#
+# IPv4 DROP POLICY
 iptables -P FORWARD DROP
 iptables -P INPUT DROP
 iptables -P OUTPUT DROP
