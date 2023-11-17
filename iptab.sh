820ace5e12f7c70db3962ee981976936524723b0
Cyber Beez <150989206+cyberbeez@users.noreply.github.com>
2023-11-16 23:25:25 -0500

    Update iptab.sh

--- iptab.sh
+++ iptab.sh
@@ -3,17 +3,16 @@ echo "Running IPTables Script from:"
 pwd
 sleep 1
 iptables-save > ./b4script.iptables.bck
+#FLUSH and REMOVE ALL RULES:
 iptables -F
+iptables -X
+iptables -Z
 #
 # IPv6 DROP POLICY
 ip6tables -P INPUT DROP
 ip6tables -P OUTPUT DROP
 ip6tables -P FORWARD DROP
 #
-#FLUSH and REMOVE ALL RULES:
-iptables -F
-iptables -X
-
 # OPEN INBOUND UNLIMITED:
 iptables -A INPUT -p udp --match multiport --dports 53 -j ACCEPT
 iptables -A INPUT -p tcp --match multiport --dports 53 -j ACCEPT
