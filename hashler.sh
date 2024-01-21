cdd30a2d76965f42ad1a7e116c6d4c46f1038c8a
Cyber Beez <150989206+cyberbeez@users.noreply.github.com>
2024-01-20 20:38:51 -0500

    Create hashler.sh

---
+++ hashler.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#This script will run a md5 hash backup and check job.
+#To use this script, change these variables:
+#file="/etc/passwd"
+file="/d/f"
+while:
+do
+md5sum $file > $file+"_hashler.md5"
+sleep 300 #5mins
+md5sum -c $file+"_hasler.md5"
+done
