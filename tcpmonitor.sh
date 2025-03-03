d9393b1b89a4e917f3525d1e29b125cc173eacf8
Cyber Beez <150989206+cyberbeez@users.noreply.github.com>
2025-03-03 16:13:51 -0500

    Update tcpmonitor.sh

--- tcpmonitor.sh
+++ tcpmonitor.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 
-counter=0
-netstat -nlp | grep SYN_RECV | while read -r line
-do
-    ((counter++))
-done
+counter='netstat -nlp | grep SYN_RECV | wc -l'
 
 echo "`date`: There were $counter incomplete TCP connections detected." >> /var/log/tcpmonitor.log
