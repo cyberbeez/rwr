756a785b7378dc374aa926e757ca248cbd701e8a
Cyber Beez <150989206+cyberbeez@users.noreply.github.com>
2025-03-03 16:41:04 -0500

    Update configmonitor.sh

--- configmonitor.sh
+++ configmonitor.sh
@@ -3,15 +3,19 @@
 files=("/etc/sysctl.conf" "/etc/security/limits.conf" "/etc/syslog.conf" "/etc/network/interfaces" "/etc/resolv.conf" "/etc/passwd" "/etc/group" "/etc/shadow" "/etc/fstab" "/etc/mtab")
 backup="./backup"
 
-while :
-do
-    for file in $files
+if [ -d $backup ]; then
+    mkdir $backup
+fi
+
+for file in $files
     do
         if [ ! -f $backup$file ]; then
             cp $file $backup$file
         fi
     done
 
+while :
+do
     for file in $files
     do
         diff $file $backup$file >> ./differences.txt
